1cd87a1ed77b3fcd82e28bfc7f7bd95ca94d9a34
Ashesh Jain <ashesh@napoli110.Stanford.EDU>
2015-08-04 07:55:41 -0700

    adding new cost

--- neuralmodels/costs.py
+++ neuralmodels/costs.py
@@ -17,6 +17,18 @@ def softmax_decay_loss(p_t,y):
 
 	shape = p_t.shape
 
+
+	def recurrence(x_t,y_t,log_loss,t):
+		log_loss_new = log_loss + T.exp(-t)*T.sum(T.log(x_t)[T.arange(y_t.shape[0]), y_t])
+		t_new = t - 1
+		return log_loss_new, t_new
+	[log_loss_list, cells], ups = theano.scan(fn=recurrence,
+					sequences=[p_t, y],
+					outputs_info=[theano.shared(value=0.0), p_t.shape[0]-1],
+					n_steps=p_t.shape[0]
+				)
+	return - (1.0/shape[1])*log_loss_list[-1]
+	'''	
 	def recurrence(x_t,t):
 		h_t = T.exp(-t)*x_t
 		t_new = t - 1
@@ -27,14 +39,8 @@ def softmax_decay_loss(p_t,y):
 					outputs_info=[None, p_t.shape[0]-1],
 					n_steps=p_t.shape[0]
 				)	
-	'''
-	exp_decay = np.ones((T,N,D)).astype(theano.config.floatX)
-	for i in range(T):
-		exp_decay[i,:,:] = np.exp(-np.log(T-i))
-	exp_decay_theano = theano.shared(value=exp_decay)
-	p_t = p_t*exp_decay_theano
-	'''
 	p_t_new = p_t_new.reshape((shape[0]*shape[1],shape[2]))
 	y = y.flatten()
 
 	return - shape[0]*(T.mean(T.log(p_t_new)[T.arange(y.shape[0]), y]))		
+	'''
